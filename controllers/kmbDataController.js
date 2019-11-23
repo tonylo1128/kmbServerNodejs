c1055a8dcca1dc98d3249cbd5a32a9151ce50bcf
tonylo1128 <tonylo19961128@gmail.com>
2019-11-23 22:47:28 +0800

    finish get time function

--- controllers/kmbDataController.js
+++ controllers/kmbDataController.js
@@ -18,35 +18,85 @@ module.exports = {
 
   getBusTime: (req, res) => {
     const inputA = req.body.inputA;
-    const inputB = req.body.inputB;
-    // console.log(inputA)
-    // console.log(inputB)
-    http.get(
-      "http://etav3.kmb.hk/?action=geteta&lang=tc&route=" +
-        inputA.route +
-        "&bound=" +
-        inputA.bound +
-        "&stop_seq=" +
-        inputB +
-        "&servicetype=" +
-        inputA.serviceType +
-        "&updatedTime=1531798892000",
-      resp => {
-        let data = "";
 
-        // A chunk of data has been recieved.
-        resp.on("data", chunk => {
-          data += chunk;
-        });
+    let resultList = [];
+    let completed_requests = 0;
 
-        // The whole response has been received. Print out the result.
-        resp.on("end", () => {
-          console.log(data);
-          res.json({
-              dataForBusData: data
+    for (let i=0; i<inputA.response.length; i++){
+      http.get(
+        "http://etav3.kmb.hk/?action=geteta&lang=tc&route=" +
+          inputA.route +
+          "&bound=" +
+          inputA.bound +
+          "&stop_seq=" +
+          inputA.response[i].Seq +
+          "&servicetype=" +
+          inputA.serviceType +
+          "&updatedTime=1531798892000",
+        resp => {
+          let data = "";
+          // A chunk of data has been recieved.
+          resp.on("data", chunk => {
+            data += chunk;
+          });
+  
+          // The whole response has been received. Print out the result.
+          resp.on("end", () => {
+            
+            let temp = JSON.parse(data)
+            console.log(temp);
+            console.log("---------------------")
+            resultList.push(temp)
+            completed_requests++;
+            if(completed_requests== inputA.response.length){
+              res.json({
+                dataForBusData:resultList
+              })
+            }
+           
           })
-        });
-      }
-    );
+          
+          
+        }
+      );
+
+    }
+    console.log("THIS IS THE END OF THE FOR LOOP")
+    console.log(resultList)
+
+    
+
+
+    // http.get(
+    //   "http://etav3.kmb.hk/?action=geteta&lang=tc&route=" +
+    //     inputA.route +
+    //     "&bound=" +
+    //     inputA.bound +
+    //     "&stop_seq=" +
+    //     inputB +
+    //     "&servicetype=" +
+    //     inputA.serviceType +
+    //     "&updatedTime=1531798892000",
+    //   resp => {
+    //     let data = "";
+
+    //     // A chunk of data has been recieved.
+    //     resp.on("data", chunk => {
+    //       data += chunk;
+    //     });
+
+    //     // The whole response has been received. Print out the result.
+    //     resp.on("end", () => {
+    //       console.log(data);
+    //       res.json({
+    //           dataForBusData: data
+    //       })
+    //     });
+    //   }
+    // );
+
+
+
+
   }
 };
